e8bc2384e9617be9a32553591045ebf0297c8853
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2005-08-28 21:52:21 +0000

    Moving over work-in-progress from laptop

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,5 +1,29 @@
 class SignupController < ApplicationController
+  before_filter :find_band
+  layout "public"
+  
   def step1
-    # Nothing to do
+ 
+  end
+  
+  def step2
+    puts @band.name
+  end
+  
+  def signup
+    @band = Band.new(params[:band])
+    puts @band.name
+    if @band.save
+      session[:band] = @band
+      redirect_to :action => 'step2'
+    else
+      render :action => 'step1'
+    end
+  end
+  
+  private
+  def find_band
+    session[:band] ||= Band.new
+    @band = session[:band]
   end
 end
--- app/models/band.rb
+++ app/models/band.rb
@@ -2,6 +2,6 @@ class Band < ActiveRecord::Base
   has_and_belongs_to_many :tags
   has_many :shows
   
-  validates_presence_of :name, :contact_email, :url
+  validates_presence_of :name, :contact_email, :url, :zipcode
   validates_uniqueness_of :name, :url
 end

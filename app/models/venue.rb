1e1362d745714adf1ea187bf4f9a7b5bc2d54b43
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2005-11-18 05:35:04 +0000

    does a little url validating.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,4 +3,28 @@ class Venue < ActiveRecord::Base
   
   validates_presence_of :name
   
+  # Set the url field, making sure it is properly qualified
+  def url=(url)
+
+    # Validate before saving
+    valid_url = validate_url(url)
+
+    write_attribute("url", valid_url)
+  end
+
+  
+  protected
+  
+  # Validate that it is a valid URL starting with http://
+  # return a validated form of it. If the url is nil, nil is returned
+  def validate_url(url)
+    
+    # Prepend right start if it is missing.    
+    if (url != nil) && !url.match(/^http/i)
+      url.insert(0, 'http://')
+    end
+  
+    return url
+  end
+  
 end

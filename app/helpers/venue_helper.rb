e33e4902f3a016bd62bd6d0489c224baf82950e6
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2005-11-19 06:08:42 +0000

    added external link to google maps

--- app/helpers/venue_helper.rb
+++ app/helpers/venue_helper.rb
@@ -9,5 +9,28 @@ module VenueHelper
     # join with a non-breaking space
     return out.join("&nbsp;")
   end
+  
+  # Return HTML to provide a link to Google Maps
+  def google_map_link(venue)
+
+    if (venue == nil)
+      return ""
+    end
+    
+    # Form the pieces that will go in the url
+    venue_str = venue.address + " " + venue.city + " " + venue.state + " " + venue.zipcode
+    
+    # Encode the URL, converting + symbols for spaces
+    url = "http://maps.google.com/maps?q=" + venue_str.split(' ').join('+')
+    
+    href = "<a href=\"#{url}\" target=\"_blank\">"
+    
+    out = href + "Full map and directions" + "</a>&nbsp;"
+    
+    out += href + "<img border=0 src=\"/images/bt-newwin.gif\"></a>"
+    
+    return out
+  end
+
 
 end

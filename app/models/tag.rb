cc099e2fda4a4fb13314a4101997932443f4aa4e
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2005-11-24 04:54:14 +0000

    in progress venue update

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,6 @@
 # A tag that is applied to a band.
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :bands
-  has_and_belongs_to_many :venues
 
   validates_presence_of :name
   validates_uniqueness_of :name

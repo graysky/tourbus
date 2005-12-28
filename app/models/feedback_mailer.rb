950b1efc85ee021edeee004542f5c10ea9577185
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2005-12-28 20:15:06 +0000

    moved the signup process a bit and added a simple feedback page.

--- app/models/feedback_mailer.rb
+++ app/models/feedback_mailer.rb
@@ -4,8 +4,8 @@ class FeedbackMailer < ActionMailer::Base
   # For when a user submits feedback about the site
   def notify_feedback(feedback, user, email, sent_at = Time.now)
 
-    # Send to all of us  
-    recipients = ['mike_champion@yahoo.com']
+    # Send to all of us - TODO Should include TB email address
+    recipients = ['mike_champion@yahoo.com', 'garypelliott@yahoo.com']
   
     @subject    = "[TourBus] New Feedback"
     @recipients = recipients

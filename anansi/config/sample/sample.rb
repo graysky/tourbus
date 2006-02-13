e4ed3bcb09c4312c924e7fb4d2b5cfcb799fb6f0
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-02-13 06:29:10 +0000

    expanded sample

---
+++ anansi/config/sample/sample.rb
@@ -0,0 +1,22 @@
+##
+## Anansi config file
+##
+
+# The URL for this site - mandatory
+# Can be single url:
+set :url, "http://figureten.com/site/test.html"
+#
+# Or an array of URLs
+# set :url, ["http://figureten.com/site/test.html", "http://figureten.com/site/test2.html"]
+#
+# Or as a Proc to embed month like "Feb" at the end
+#set(:url)   { "http://figureten.com/page/#{ Time.now.strftime("%b") }" }
+#
+
+# A set of table columns
+set :table_columns, { 0 => :date, 1 => :time, 2 => :bands, 
+                  3 => :venue, 4 => :age_limit, 5 => :cost, 6 => :ignore }
+
+# How often (in hours) to check the site                  
+set :interval, 24
+

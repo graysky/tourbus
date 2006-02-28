8c14fcfa7b90258afff8233c793112ab8c1e61b4
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-02-28 17:14:19 +0000

    First attempt at pulling out extra information and preambles from band listings.

--- anansi/lib/show_parser.rb
+++ anansi/lib/show_parser.rb
@@ -26,47 +26,86 @@ class ShowParser
     raise "Implement this method in the subclass"
   end
   
+  # Returns an array of the prepared band name and the extra content
   def prepare_band_name(name)
     # Remove ampersand if it begins with it
-    name.gsub(/&/, '') if name[0..0] == "&"
-    name.gsub(/&amp;/, '') if name[0..4] == "&amp;"
+    name.gsub!(/&/, '') if name[0..0] == "&"
+    name.gsub!(/&amp;/, '') if name[0..4] == "&amp;"
+    
+    extra = nil
     
     # Remove parts in parens
-    name = name.gsub(/\((.)*\)/, '')
+    name = name.gsub(/(\((.)*\))/, '')
+    extra = $1 if not $1.nil?
     
     # Remove parts in brackets
-    name = name.gsub(/\[(.)*\]/, '')
+    name = name.gsub(/(\[(.)*\])/, '')
+    extra = $1 if not $1.nil?
     
     # Remove everything after a dash if there is whitespace around it.
     index = name.index(" - ")
+    extra = name[index, name.length] if index
     name = name[0, index] if index
     
-    name.strip
+    extra.strip! if not extra.nil?
+    [name.strip, extra]
   end
   
   # Index is there because certain phrases are more likely to mean there is not a band playing
   # TODO We need a config option so that sites can override this. People do wacky things with band listings!
-  def probable_band(name, index, extra = nil)
-    downcase = name.downcase
+  def probable_band(name, index, starting_preamble = nil)
     return nil if name.nil? or name.strip == ""
     
-    name = prepare_band_name(name)
+    name, extra = prepare_band_name(name)
+    preamble = nil
     
-    # This list will grow a lot over time... this is very basic.
-    # Not all of these will necessarily apply to non-table layouts. Will need to figure out
-    # how to handle all the different cases.
-    # TODO take everything AFTER : or present or with ALSO DO "Featuring"
-    return nil if index == 0 and downcase.include?(":")
-    return nil if index == 0 and downcase.ends_with?("presents")
-    return nil if index == 0 and downcase.ends_with?(" with")
-    return nil if name.ends_with?(":")
-    return nil if downcase.include?("tba") or downcase.include?("t.b.a") # Too restrictive? Not many words with tba.
-    return nil if name.split(" ").size > 12 # more like a paragraph than a band name
-    return nil if downcase.ends_with?("and more...") or downcase.ends_with?("and more") or downcase.ends_with?("more...")
-    return nil if downcase.include?("special guest")
-    return nil if downcase.include?("+")
+    if index == 0
+      name, preamble = process_first_band(name)
+      return nil if name.nil?
+    end
     
-    return name
+    down = name.downcase
+    
+    return nil if down.ends_with?(":")
+    return nil if down.include?("tba") or down.include?("t.b.a") # Too restrictive? Not many words with tba.
+    return nil if down.split(" ").size > 10 # more like a paragraph than a band name
+    return nil if down.ends_with?("and more...") or down.ends_with?("and more") or down.ends_with?("more...")
+    return nil if down.include?("special guest")
+    return nil if down.include?("many more")
+    return nil if down.include?("+")
+    
+    band = {}
+    band[:name] = name
+    band[:extra] = extra if extra
+    band[:preamble] = preamble if preamble
+    
+    band
+  end
+  
+  def process_first_band(name, preamble = nil)
+    down = name.downcase
+    
+    # The first one is tricky because it could have some preamble that
+    # we need to strip off and save, and still get the headlining band.
+    # But first, if we end with these strings than this entire first chunk
+    # is just a preamble. TODO We should probably save it
+    return nil if down.ends_with?(" with")
+    return nil if down.ends_with?("presents")
+    return nil if down.ends_with?("present")
+    return nil if down.ends_with?("featuring")
+    return nil if down.ends_with?("special guest")
+    return nil if down.ends_with?(":")
+    
+    keywords = [":", "featuring", "presents", "presenting", "special guest"]
+    index = first_index_in(keywords, down)
+    if index
+      new_preamble = name[0..index].strip
+      new_preamble = preamble + ' ' + new_preamble if preamble
+      name = name[index, name.length]
+      name, preamble = process_first_band(name, new_preamble)
+    end
+    
+    return name.strip, preamble.nil? ? nil : preamble.strip
   end
   
   # Parse the given string as a date
@@ -79,7 +118,7 @@ class ShowParser
     
     # Need at least a month and a date. Assume this year (for now)
     if values[1].nil? or values[2].nil?
-      raise "Bad date: #{contents}" if raise_on_error
+      raise "Bad date: #{str}" if raise_on_error
       return nil
     end
     
@@ -93,6 +132,17 @@ class ShowParser
   end
   
   private
+  
+  # Return the index of the first choice that is found in str
+  # plus the length of the string
+  def first_index_in(choices, str)
+    choices.each do |choice|
+      index = str.index(choice)
+      return index + choice.length if index
+    end
+    
+    nil
+  end
   
   # Define a new method on this object
   def define_method(name, &block)
--- anansi/test/unit/test_show_parser.rb
+++ anansi/test/unit/test_show_parser.rb
@@ -10,5 +10,45 @@ class ShowParserTest < Test::Unit::TestCase
   
   def test_parse_as_date
     assert_date(2, 2, 2006, @parser.parse_as_date("2/2/06"))
+    assert_date(2, 2, 2006, @parser.parse_as_date("2.2.06"))
+    assert_date(2, 2, 2006, @parser.parse_as_date("02-02-06"))
+    assert_date(2, 2, 2006, @parser.parse_as_date("02-02"))
+    assert_date(2, 2, 2006, @parser.parse_as_date("02-02-2006 7pm"))
+    assert_date(5, 10, 2006, @parser.parse_as_date("May 10"))
+    assert_date(5, 10, 2006, @parser.parse_as_date("May 10, 06"))
+    assert_date(5, 10, 2006, @parser.parse_as_date("May 10 2006"))
+    
+    assert_nil(@parser.parse_as_date("not a date", false))
+    assert_raise(RuntimeError) { @parser.parse_as_date("not a date") }
+  end
+  
+  def test_prepare_band_name
+    assert_equal(["endgame", nil], @parser.prepare_band_name("& endgame"))
+    assert_equal(["endgame", nil], @parser.prepare_band_name("endgame"))
+    assert_equal(["crisis bureau", nil], @parser.prepare_band_name("crisis bureau  "))
+    assert_equal(["crisis bureau", "(ex-endgame)"], 
+                 @parser.prepare_band_name("crisis bureau (ex-endgame)"))
+    assert_equal(["crisis bureau", "[ex-endgame]"], 
+                 @parser.prepare_band_name("crisis bureau [ex-endgame]"))
+    assert_equal(["crisis bureau", "- one night only!"], 
+                 @parser.prepare_band_name("crisis bureau - one night only!"))
+  end
+  
+  def test_process_first_band
+    assert_equal(["endgame", nil], @parser.process_first_band("endgame"))
+    assert_equal(["crisis bureau (ex-endgame)", nil], @parser.process_first_band("crisis bureau (ex-endgame)"))
+    
+    assert_equal(nil, @parser.process_first_band("A show featuring"))
+    assert_equal(nil, @parser.process_first_band("Gasry productions presents:"))
+    assert_equal(nil, @parser.process_first_band("Gasry productions presents"))
+    assert_equal(nil, @parser.process_first_band("Mike and Gary present"))
+    assert_equal(nil, @parser.process_first_band("all the way from england a very special guest"))
+    
+    assert_equal(["endgame", "a show featuring"], @parser.process_first_band("a show featuring endgame"))
+    assert_equal(["crisis bureau", "a record label presents:"], @parser.process_first_band("a record label presents: crisis bureau"))
+    assert_equal(["crisis bureau", "webster presents a show featuring"], 
+                 @parser.process_first_band("webster presents a show featuring crisis bureau"))
+    assert_equal(["crisis bureau", "webster featuring a show presents"], 
+                 @parser.process_first_band("webster featuring a show presents crisis bureau"))
   end
 end

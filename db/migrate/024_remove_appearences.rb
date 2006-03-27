e477632e7bb185f670b2c176b9b925054022a395
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-03-27 05:37:37 +0000

    renamed 24 to 25

--- db/migrate/024_remove_appearences.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAppearences < ActiveRecord::Migration
-  def self.up
-    rename_table :appearences, :bands_shows
-  end
-
-  def self.down
-    rename_table :bands_shows, :appearences
-  end
-end

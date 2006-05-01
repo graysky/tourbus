cf452d96436599b86c76f4f9d34b86ea2fbaac1e
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-05-01 04:53:08 +0000

    minor changes

--- app/helpers/venue_helper.rb
+++ app/helpers/venue_helper.rb
@@ -2,7 +2,7 @@ module VenueHelper
 
   def venue_desc
     if @venue.description != ""
-      "<div id='venue_desc'>#{@venue.description}</div>"
+      "<div id='venue_desc'>#{h(@venue.description)}</div>"
     else
       "<div id='venue_desc'></div>"
     end
@@ -11,6 +11,7 @@ module VenueHelper
   def venue_url
     if @venue.url != ""
       link = @venue.url.sub(/http:\/\//, '') # Shorten link to remove "http://"
+      link.sub!(/\/$/, '') # Remove trailing slash
       html = "<div id='venue_url'><a href='#{@venue.url}'>#{link}</a></div>"
     else
       "<div id='venue_url'></div>"

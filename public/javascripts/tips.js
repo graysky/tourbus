3a11a82f417594706af5d80575d166e11a4b015e
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-05-02 20:59:53 +0000

    Fix tooltips so that when you click on one, you can use the one that replaces it (add vs. remvoe attending, for example)

---
+++ public/javascripts/tips.js
@@ -0,0 +1,29 @@
+function addAttending(event)
+{
+  Tooltip.toggle($('add_attending_link'), event);
+}
+
+function removeAttending(event)
+{
+  Tooltip.toggle($('remove_attending_link'), event);
+}
+
+function addWatching(event)
+{
+  Tooltip.toggle($('add_watching_link'), event);
+}
+
+function removeWatching(event)
+{
+  Tooltip.toggle($('remove_watching_link'), event);
+}
+
+function addFave(event)
+{
+  Tooltip.toggle($('add_fave_link'), event);
+}
+
+function removeFave(event)
+{
+  Tooltip.toggle($('remove_fave_link'), event);
+}

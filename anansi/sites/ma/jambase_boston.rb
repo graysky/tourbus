8c8a46e551f7d29dbe56af3a6171cf2c97fe1d01
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-05-13 15:34:51 +0000

    boston jambase

---
+++ anansi/sites/ma/jambase_boston.rb
@@ -0,0 +1,32 @@
+## 
+## Anansi config file for The Middle East in Cambridge
+#
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+
+#
+# The URL for this site - mandatory
+# Can be single url:
+set :url, "http://www.jambase.com/search.asp?city=Boston&stateID=21&dispall=1"
+#
+
+# How often (in hours) to check the site (can set to 0 to force checking everytime)
+set :interval, 72
+
+# Use the table parser 
+set :parser_type, :jambase
+
+# =============================================================================
+# OPTIONAL VARIABLES
+# =============================================================================
+#
+
+# =============================================================================
+# DEFINE METHODS
+# =============================================================================
+#
+# All shows for this site are at the same venue
+method :default_time do
+  "7pm"
+end

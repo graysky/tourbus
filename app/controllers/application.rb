04ed574cb3c7ed21dea158e8182fd3615481af43
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-06-15 02:57:07 +0000

    testing ical

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -40,7 +40,7 @@ class ApplicationController < ActionController::Base
   before_filter :configure_charsets
   before_filter :login_from_cookie
   # Check for beta invite cookie in the public controller
-  before_filter :beta_cookie, :except => [:beta, :beta_signup, :rss, :friends_shows_rss, :ical, :external_map, :badge]
+  before_filter :beta_cookie, :except => [:beta, :beta_signup, :rss, :friends_shows_rss, :ical, :ical2, :external_map, :badge]
 
   # Use UTF charsets. From:
   # http://wiki.rubyonrails.org/rails/pages/HowToUseUnicodeStrings

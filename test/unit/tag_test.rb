bb43f8a196736babd0b534d2029b39130a4411f3
champion <champion@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-06-25 18:34:31 +0000

    couple tag tests

--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -1,14 +1,20 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TagTest < Test::Unit::TestCase
-  #fixtures :tags
+  fixtures :tags
 
   def setup
-    #@tag = Tag.find(1)
+    @punk = tags(:punk)
   end
 
   # Replace this with your real tests.
-  def test_truth
-    #assert_kind_of Tag,  @tag
+  def test_valid
+    unnamed_tag = Tag.new
+    unnamed_tag.name = nil
+    assert(!unnamed_tag.valid?, "Invalid tag")
+  
+    dup_tag = Tag.new
+    dup_tag.name = "punk"
+    assert(!dup_tag.valid?, "Invalid tag")
   end
 end

d2f34409a50cd2d501fc6b11600a208a78386b71
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-07-04 04:50:18 +0000

    jambase la

---
+++ anansi/sites/ca/jambase_la.rb
@@ -0,0 +1,31 @@
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+
+#
+# The URL for this site - mandatory
+# Can be single url:
+set :url, "http://www.jambase.com/search.asp?city=Los%20Angeles&stateID=5&beyondCity=1"
+#
+set :display_name, "Jambase"
+set :quality, 1
+
+# How often (in hours) to check the site (can set to 0 to force checking everytime)
+set :interval, 72
+
+# Use the table parser 
+set :parser_type, :jambase
+
+# =============================================================================
+# OPTIONAL VARIABLES
+# =============================================================================
+#
+
+# =============================================================================
+# DEFINE METHODS
+# =============================================================================
+#
+# All shows for this site are at the same venue
+method :default_time do
+  "7pm"
+end

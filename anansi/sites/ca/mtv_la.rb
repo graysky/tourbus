e4475f402cc548d545315ba6292437afcf32decf
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-07-04 04:42:39 +0000

    mtv la

---
+++ anansi/sites/ca/mtv_la.rb
@@ -0,0 +1,39 @@
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+
+#
+# The URL for this site - mandatory
+# Can be single url:
+set :url, "http://www.mtv.com/music/tours/results.jhtml?city=Los%20Angeles&countryID=145"
+
+#
+set :display_name, "MTV"
+# How often (in hours) to check the site (can set to 0 to force checking everytime)
+set :interval, 72
+
+set :quality, 2
+
+# Use the table parser 
+set :parser_type, :table
+
+# =============================================================================
+# OPTIONAL VARIABLES
+# =============================================================================
+#
+
+# Table columns for the shows
+set :table_columns, { 0 => :date, 2 => :bands, 4 => :venue }
+
+# Comma separates bands
+set :band_separator, '/'
+
+set :marker_text, 'Hollywood'
+
+# =============================================================================
+# DEFINE METHODS
+# =============================================================================
+#
+method :default_time do
+  "7pm"
+end

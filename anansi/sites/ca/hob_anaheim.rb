4dda443650aceb1cd6df755ed8d95505921920a0
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-07-18 03:49:58 +0000

    anansi updates in CA

---
+++ anansi/sites/ca/hob_anaheim.rb
@@ -0,0 +1,18 @@
+#
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+
+#
+# The URL for the site
+set :url, "http://www.livenation.com/feed/venuefeed/venueid/10516"
+
+set :display_name, "Live Nation"
+set :parser_type, :live_nation
+set :quality, 3
+set :xml, true
+
+# Define "venue_id" in the site.rb file
+method :get_venue do
+  { :id => 926 } 
+end
---
+++ anansi/sites/ca/wiltern_lg.rb
@@ -0,0 +1,18 @@
+#
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+
+#
+# The URL for the site
+set :url, "http://www.livenation.com/feed/venuefeed/venueid/1237"
+
+set :display_name, "Live Nation"
+set :parser_type, :live_nation
+set :quality, 3
+set :xml, true
+
+# Define "venue_id" in the site.rb file
+method :get_venue do
+  { :id => 996 } 
+end

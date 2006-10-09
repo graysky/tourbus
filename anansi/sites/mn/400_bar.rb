29f5e29d9a579ae640e0c2ee8844803acc09b9a0
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2006-10-09 15:33:50 +0000

    sort of pathetic support for MN, but a start

---
+++ anansi/sites/mn/400_bar.rb
@@ -0,0 +1,21 @@
+#
+# =============================================================================
+# REQUIRED VARIABLES
+# =============================================================================
+
+#
+# The URL for the site
+set :url, "http://www.ticketweb.com/user/?region=mn&query=schedule&venue=400bar"
+
+set :display_name, "TicketWeb"
+set :parser_type, :ticket_web
+set :quality, 3
+
+# Define "venue_id" in the site.rb file
+method :get_venue do
+  { :id => 1441 } 
+end
+
+method :links_to_follow, {:args => 1} do |xml_doc|
+  TicketWebParser.links_to_follow(xml_doc)
+end

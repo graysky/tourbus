645f50c667bd8c3da96e3eb8ccc9a7b2b123a6a9
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2007-02-15 05:40:41 +0000

    hit form updates

--- app/controllers/turkers_controller.rb
+++ app/controllers/turkers_controller.rb
@@ -9,7 +9,7 @@ class TurkersController < ApplicationController
       render :partial => "hit_error", :layout => "turk_hit"
       return
     end
-      
+     
     if request.get?
       # We must be showing the form
       @assignmentId = params[:assignmentId]  
@@ -28,33 +28,34 @@ class TurkersController < ApplicationController
     @invalid_bands = []
     show_count = 0
     parser = ShowParser.new(nil)
+    last_index = 99
     
-    for i in (0..100)
-      date = params["date#{i}"]
-      bands = params["bands#{i}"]
+    for i in (0...100)
+      date = params["date_#{i}"]
+      bands = params["bands_#{i}"]
       
-      next if (date.blank? && bands.blank?)
+      if (date.blank? && bands.blank?)
+        last_index = i
+        break
+      end
       
       if (d = DateUtils.parse_date_loosely(date)).nil?
         @errors << [i, "Invalid date: #{date}. The expected format is MM/DD/YY"]
         next
       end
       
-      puts "date: #{d}"
-      
-      tickets = params["tickets#{i}"]
+      tickets = params["tickets_#{i}"]
       if !tickets.blank? && !tickets.downcase.starts_with?("http://")
         @errors << [i, "Invalid ticket link : \"#{tickets}\". Must start with http://"]
         next
       end
       
-      bands = params["bands#{i}"].to_s.gsub(/\r/, '').split(/\n/)
+      bands = params["bands_#{i}"].to_s.gsub(/\r/, '').split(/\n/)
       index = 0
       bands_this_show = []
       for band in bands
         b = parser.probable_band(band, index, nil)
         if b
-          puts "  band: #{b}"
           bands_this_show << b
         else
           @invalid_bands << [i, band]
@@ -70,7 +71,6 @@ class TurkersController < ApplicationController
       show_count += 1
     end
     
-    
     if show_count == 0
       @errors << [nil, "We did not find any valid shows. Did you hit submit by accident?"]
     elsif (@invalid_bands.size / show_count.to_f) < 0.4
@@ -78,11 +78,22 @@ class TurkersController < ApplicationController
       @invalid_bands = []
     end
     
-    if @errors.size > 0 || @invalid_bands
+    if @errors.size > 0 || @invalid_bands.size > 0
       render :layout => "turk_hit"
     else
-      flash.now[:success] = "success"
-      render :layout => "turk_hit"
+      url = "http://www.mturk.com/mturk/externalSubmit?assignmentId=100&"
+      param_str = ""
+      
+      params.each do |key, value|
+        chunks = key.split("_")
+        if (chunks[1].nil? || chunks[1].to_i < last_index)
+          param_str += "#{key}=#{CGI::escape(value.to_s)}&"
+        end
+      end
+      
+      url += param_str
+      logger.info("Hit complete for site: #{@site.id}. Url: #{url}")
+      redirect_to(url)
     end
   end
   

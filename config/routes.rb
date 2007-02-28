21a0d25fd13ddadf9886f64d107c3c2b05338573
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2007-02-28 01:25:17 +0000

    routing fix (I think)

--- config/routes.rb
+++ config/routes.rb
@@ -89,7 +89,9 @@ ActionController::Routing::Routes.draw do |map|
     
   # Public band pages (must be last)
   map.connect ':short_name/:action/:id', :controller => 'band_public'
+  map.connect ':short_name/:action/:id', :controller => 'band_public', :requirements => { :id => /.*/ }
 
   # Install the default route as the lowest priority.
+  map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
 end

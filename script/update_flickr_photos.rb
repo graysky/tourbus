d3c470a2c3ccb6e5f39201407af01e7b8ddf3f81
gary <gary@fb8397f0-9209-0410-8406-d61aaa80b2ea>
2007-04-19 03:45:00 +0000

    flickr update script

---
+++ script/update_flickr_photos.rb
@@ -0,0 +1,14 @@
+require 'config/environment'
+
+ps = PhotoSearch.new
+
+bands = Band.find(:all, :order => "num_fans desc", :limit => 400)
+
+bands.each do |b|
+  begin
+    ps.find_show_photos(b, 1.week.ago)
+  rescue Exception => e
+    OFFLINE_LOGGER.error e.to_s
+    OFFLINE_LOGGER.error e.backtrace
+  end
+end
